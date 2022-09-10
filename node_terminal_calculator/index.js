538c9e791cbc647ab0fa2bd9b0b517264fce3b3a
sugaith <sugaith@gmail.com>
2022-09-10 19:21:00 -0300

    clean

--- node_terminal_calculator/index.js
+++ node_terminal_calculator/index.js
@@ -5,15 +5,14 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
-
-console.log('Welcome to the Node Calculator!')
-console.log(`Type 'exit' or Ctrl+C to exit`)
-console.log('______________________________')
-
 reader.on('close', () => {
   console.log('\n BYE BYE !!!')
   process.exit(0);
 })
+
+console.log('Welcome to the Node Calculator!')
+console.log(`Type 'exit' or Ctrl+C to exit`)
+console.log('______________________________')
 
 const calculator = new Calculator(reader)
 
